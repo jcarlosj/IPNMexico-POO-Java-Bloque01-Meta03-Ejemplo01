34ae07f4f663df1ea0602a34712d8a577d4408ed
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-12 22:45:28 -0500

    Agrega funcionalidad final al proyecto
     - Agrega un ciclo "while" para validar si la edad permitida es
       mayor o igual a 18 y en caso de no serlo incrementarla hasta
       que esta se cumpla para asignar la licencia.

--- src/edx/ipnmexico/b1m3e1/MiProyecto.java
+++ src/edx/ipnmexico/b1m3e1/MiProyecto.java
@@ -4,7 +4,17 @@ public class MiProyecto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String nombre = "Juanita";
+		int edad = 13;
+		
+		//-> Ciclo que incrementa la edad en uno.
+		while( !validarEdad( edad ) ) {
+			edad = incrementarEdad( edad );
+		}
+		
+		System .out .println( "Licencia otorgada a " + nombre );
+		
 	}
 
 	//-> Valida la edad permitida para la expedición de la licencia de conducir.
@@ -21,6 +31,7 @@ public class MiProyecto {
 		
 	}
 	
+	//-> Incrementa la edad en uno.
 	public static int incrementarEdad( int edad ) {
 		return edad + 1;
 	}
